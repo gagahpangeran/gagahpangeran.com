f7826c7133f132a1513548d5d6a2c40564f0e254
Gagah Pangeran <gagahpangeran@gmail.com>
2019-06-03 14:27:09 +0700

    setting to connect wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
+require("dotenv").config()
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gagah Pangeran Rosfatiputra`,
+    description: `Part Time Student, Full Time Learner`,
+    author: `GPR`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +27,16 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: process.env.WORDPRESS_BASE_URL,
+        protocol: process.env.WORDPRESS_PROTOCOL,
+        hostingWPCOM: process.env.WORDPRESS_HOSTING_WPCOM === "true",
+        useACF: process.env.WORDPRESS_USE_ACF === "true",
+        includedRoutes: ["**/posts", "**/pages", "**/tags"],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

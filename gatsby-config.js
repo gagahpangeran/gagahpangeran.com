52f5f9dcc320a46dd03ef9e66209aebde2ec1197
Gagah Pangeran <gagahpangeran@gmail.com>
2019-06-04 15:23:11 +0700

    add logo gpr

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
-require("dotenv").config()
+require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
     title: `Gagah Pangeran Rosfatiputra`,
     description: `Part Time Student, Full Time Learner`,
-    author: `GPR`,
+    author: `@gagahpangeran_`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -23,10 +23,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     {
@@ -43,4 +43,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};

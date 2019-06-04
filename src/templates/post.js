2a875ac440b5997a77a9da958fb0a50328498a04
Gagah Pangeran <gagahpangeran@gmail.com>
2019-06-04 15:44:25 +0700

    change format in post

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Layout from "../components/layout";
+import SEO from "../components/seo";
 import { graphql } from "gatsby";
 
 export default ({ data }) => {
   const post = data.wordpressPost;
+  const desc = post.excerpt.substring(3, post.excerpt.length - 4);
+
   return (
     <Layout>
+      <SEO title={post.title} description={desc} />
       <div>
         <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
-        <h3>date: {post.date}</h3>
+        <h3>{post.date}</h3>
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
       </div>
     </Layout>
@@ -19,7 +23,8 @@ export const pageQuery = graphql`
   query($id: String!) {
     wordpressPost(id: { eq: $id }) {
       title
-      date(formatString: "DD MMMM YYYY")
+      date(formatString: "D MMM YYYY, HH.mm")
+      excerpt
       content
     }
   }

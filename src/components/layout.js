597fa41190a1d48c68b96ad7b7d4f5285e2da87d
Gagah Pangeran <gagahpangeran@gmail.com>
2019-06-04 16:47:40 +0700

    reset style

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,12 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
 
-import Header from "./header"
-import "./layout.css"
+import Navbar from "./navbar";
+import "../styles/index.css";
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -25,7 +25,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        <Navbar />
         <div
           style={{
             margin: `0 auto`,
@@ -44,10 +44,10 @@ const Layout = ({ children }) => (
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;

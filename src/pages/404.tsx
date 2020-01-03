141c419e27bd6d501e3992a339e65adb6beb58ca
Gagah Pangeran <gagahpangeran@gmail.com>
2020-01-03 16:18:36 +0700

    modify 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,39 @@
 import React from "react";
 import { Link } from "gatsby";
+import styled from "../utils/styled";
+
+import shinobu from "../images/shinobu.gif";
 
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 
+const PageDesc = styled.h3`
+  color: ${props => props.theme.gray.light};
+`;
+
+const PageLink = styled(Link)`
+  font-size: 20px;
+  display: block;
+  text-align: center;
+  margin: 16px 0;
+`;
+
+const PageImage = styled.img`
+  display: block;
+  margin: 0 auto;
+  max-width: 100%;
+  height: auto;
+`;
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="Not found" />
-    <h1>NOT FOUND</h1>
-    <p>Sorry, the page doesn't exist (or maybe I haven't build it yet :P )</p>
-    <Link to="/">Back to home</Link>
+    <h1>Sorry, not found :'(</h1>
+    <PageDesc>
+      Sorry, the page doesn't exist (or maybe I haven't build it yet :P )
+    </PageDesc>
+    <PageLink to="/">Back to home</PageLink>
+    <PageImage src={shinobu} alt="Cute Shinobu" />
   </Layout>
 );
 

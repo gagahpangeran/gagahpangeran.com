e0a826aa6b1a970a0a018c4d63a032c3b1aa02ee
Gagah Pangeran <git@gagahpangeran.com>
2021-02-16 13:20:02 +0700

    revamp blog template

--- src/templates/Blog.tsx
+++ src/templates/Blog.tsx
@@ -1,146 +1,54 @@
+import { graphql, PageProps } from "gatsby";
 import React from "react";
-import { graphql } from "gatsby";
-import styled from "../utils/styled";
-
+import { BlogTemplate } from "../../types/generated-types";
 import Layout from "../components/Layout";
+import PostCard from "../components/PostCard";
+import PostThumbnail from "../components/PostThumbnail.old";
 import SEO from "../components/SEO";
 
-import PostThumbnail from "../components/PostThumbnail";
-import Pagination from "../components/Pagination";
-
-const PageDesc = styled.h3`
-  color: ${props => props.theme.gray.light};
-`;
-
-const resolveData = (data: any, type: string) => {
-  switch (type) {
-    case "Blog":
-      return {
-        pageTitle: "Gagah Pangeran Rosfatiputra",
-        pageDesc: "Part Time Student, Full Time Learner",
-        title: "Blog",
-        desc: "Part Time Student, Full Time Learner",
-        post: data.allPost.edges
-      };
-    case "Category":
-      return {
-        pageTitle: data.wordpressCategory.name,
-        pageDesc: `Show All Posts Under Category "${data.wordpressCategory.name}"`,
-        title: `Category "${data.wordpressCategory.name}"`,
-        desc: `All Posts Under Category "${data.wordpressCategory.name}"`,
-        post: data.allCategoryPost.edges
-      };
-    case "Tag":
-      return {
-        pageTitle: data.wordpressTag.name,
-        pageDesc: `Show All Posts Under Tag "${data.wordpressTag.name}"`,
-        title: `Tag "${data.wordpressTag.name}"`,
-        desc: `All Posts Under Tag "${data.wordpressTag.name}"`,
-        post: data.allTagPost.edges
-      };
-    default:
-      return data;
-  }
-};
-
-export default function Blog({
-  data,
-  pageContext
-}: {
-  data: any;
-  pageContext: any;
-}) {
-  const { title, desc, post, pageTitle, pageDesc } = resolveData(
-    data,
-    pageContext.type
-  );
+const Blog: React.FC<PageProps<BlogTemplate>> = ({ data }) => {
+  const posts = data.posts.nodes;
 
   return (
     <Layout>
-      <SEO title={title} description={desc} />
-      <h1>{pageTitle}</h1>
-      <PageDesc>{pageDesc}</PageDesc>
+      <SEO title="Blog" description="Part Time Student, Full Time Learner" />
+      <h1>GPR's Blog</h1>
+      <h2>Part Time Student, Full Time Learner.</h2>
 
       <main>
-        {post.map(({ node }: { node: any }) => (
-          <PostThumbnail key={node.id} {...node} />
-        ))}
+        {posts.map(post => {
+          const title = post.frontmatter?.title ?? "";
+          const date = post.frontmatter?.date ?? "";
+          const excerpt = post.frontmatter?.description ?? post.excerpt ?? "";
+          const slug = post.fields?.slug ?? "";
+
+          return (
+            <PostCard title={title} date={date} excerpt={excerpt} slug={slug} />
+          );
+        })}
       </main>
-      {pageContext.type === "Blog" && (
-        <Pagination
-          page={pageContext.page}
-          numPages={pageContext.numPages}
-          path=""
-        />
-      )}
     </Layout>
   );
-}
+};
+
+export default Blog;
 
 export const pageQuery = graphql`
-  query($id: String!, $skip: Int, $limit: Int) {
-    wordpressCategory(id: { eq: $id }) {
-      name
-    }
-
-    wordpressTag(id: { eq: $id }) {
-      name
-    }
-
-    allPost: allWordpressPost(
-      sort: { fields: [date], order: DESC }
-      limit: $limit
+  query BlogTemplate($skip: Int, $limit: Int) {
+    posts: allMarkdownRemark(
+      sort: { fields: frontmatter___date, order: DESC }
       skip: $skip
+      limit: $limit
     ) {
-      ...postDetail
-    }
-
-    allCategoryPost: allWordpressPost(
-      filter: { categories: { elemMatch: { id: { eq: $id } } } }
-      sort: { order: DESC, fields: [date] }
-    ) {
-      ...postDetail
-    }
-
-    allTagPost: allWordpressPost(
-      filter: { tags: { elemMatch: { id: { eq: $id } } } }
-      sort: { order: DESC, fields: [date] }
-    ) {
-      ...postDetail
-    }
-  }
-
-  fragment postDetail on wordpress__POSTConnection {
-    totalCount
-    edges {
-      node {
-        id
-        title
-        excerpt
-        slug
-        date
-        categories {
-          name
+      nodes {
+        excerpt(pruneLength: 160)
+        fields {
           slug
         }
-        tags {
-          name
-          slug
-        }
-        featured_media {
-          localFile {
-            childImageSharp {
-              fluid(maxWidth: 800) {
-                base64
-                aspectRatio
-                src
-                srcSet
-                srcWebp
-                srcSetWebp
-                sizes
-              }
-            }
-          }
+        frontmatter {
+          title
+          description
+          date(formatString: "MMMM DD, YYYY")
         }
       }
     }

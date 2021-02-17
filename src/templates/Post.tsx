b030e762f20c7f80b5e7dbd19210a0728a3d9616
Gagah Pangeran <git@gagahpangeran.com>
2021-02-17 13:41:52 +0700

    use Clock Icon from font awesome

--- src/templates/Post.tsx
+++ src/templates/Post.tsx
@@ -1,3 +1,5 @@
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { graphql, PageProps } from "gatsby";
 import React from "react";
 import { PostTemplate } from "../../types/generated-types";
@@ -24,8 +26,8 @@ const Post: React.FC<PageProps<PostTemplate>> = ({ data }) => {
         <article className="post">
           <h1 className="post__title">{title}</h1>
           <time className="post__date">
-            <ClockIcon />
-            {date}
+            <FontAwesomeIcon icon={faClock} />
+            <span>{date}</span>
           </time>
           <CategoryTags category={category} tags={tags} />
           <ShareButton link={`${siteUrl}${slug}`} />
@@ -66,19 +68,3 @@ export const pageQuery = graphql`
     }
   }
 `;
-
-const ClockIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24pt"
-      height="24pt"
-      viewBox="0 0 24 24"
-      version="1.1"
-    >
-      <g id="surface1">
-        <path d="M 12 0.375 C 5.578125 0.375 0.375 5.578125 0.375 12 C 0.375 18.421875 5.578125 23.625 12 23.625 C 18.421875 23.625 23.625 18.421875 23.625 12 C 23.625 5.578125 18.421875 0.375 12 0.375 Z M 14.675781 16.785156 L 10.542969 13.78125 C 10.398438 13.671875 10.3125 13.503906 10.3125 13.328125 L 10.3125 5.4375 C 10.3125 5.128906 10.566406 4.875 10.875 4.875 L 13.125 4.875 C 13.433594 4.875 13.6875 5.128906 13.6875 5.4375 L 13.6875 11.890625 L 16.664062 14.058594 C 16.917969 14.242188 16.96875 14.59375 16.785156 14.84375 L 15.464844 16.664062 C 15.28125 16.914062 14.929688 16.96875 14.675781 16.785156 Z M 14.675781 16.785156 " />
-      </g>
-    </svg>
-  );
-};

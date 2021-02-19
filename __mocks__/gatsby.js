a1adda9e9f59b405d5be9829c8fa49deb2d1be37
Gagah Pangeran <git@gagahpangeran.com>
2021-02-19 10:08:51 +0700

    fix mock file

--- __mocks__/gatsby.js
+++ __mocks__/gatsby.js
@@ -1,15 +1,37 @@
-const React = require(`react`);
-const gatsby = jest.requireActual(`gatsby`);
+const React = require("react");
+const gatsby = jest.requireActual("gatsby");
 
 module.exports = {
   ...gatsby,
   graphql: jest.fn(),
-  Link: jest.fn().mockImplementation(({ to, ...rest }) =>
-    React.createElement(`a`, {
-      ...rest,
-      href: to
-    })
-  ),
+  Link: jest
+    .fn()
+    .mockImplementation(
+      ({
+        activeClassName,
+        activeStyle,
+        getProps,
+        innerRef,
+        partiallyActive,
+        ref,
+        replace,
+        to,
+        ...rest
+      }) =>
+        React.createElement("a", {
+          ...rest,
+          href: to
+        })
+    ),
   StaticQuery: jest.fn(),
-  useStaticQuery: jest.fn()
+  useStaticQuery: jest.fn().mockImplementation(_ => ({
+    site: {
+      siteMetadata: {
+        title: `Gagah Pangeran Rosfatiputra`,
+        description: `Part Time Student, Full Time Learner`,
+        image: "logo.png",
+        siteUrl: "https://blog.gagahpangeran.com"
+      }
+    }
+  }))
 };

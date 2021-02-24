be1c091de245bc2c5315ddd6a9cf685b7ef6c379
Gagah Pangeran <git@gagahpangeran.com>
2021-02-24 13:21:54 +0700

    add comment notice in gatsby-node and gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,8 @@
 // Read the LICENSE file in the repository root for full license text.
 
 /* eslint-disable @typescript-eslint/no-var-requires */
+
+// This file is just a wrapper, please see and edit file `gatsby-config.ts`.
 require("source-map-support").install();
 require("ts-node").register({ files: true, transpileOnly: true });
 
--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,8 @@
 // Read the LICENSE file in the repository root for full license text.
 
 /* eslint-disable @typescript-eslint/no-var-requires */
+
+// This file is just a wrapper, please see and edit file `gatsby-node.ts`.
 require("source-map-support").install();
 require("ts-node").register({ files: true, transpileOnly: true });
 

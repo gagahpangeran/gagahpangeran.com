44bab1dfdb5619d79572b93edd9fd7999ed27637
Gagah Pangeran <git@gagahpangeran.com>
2021-03-13 23:45:06 +0700

    move out test data to global variable
    I re-commit this and all commit after this to clean up the commit
    history as mentioned in https://github.com/gagahpangeran/blog.gagahpangeran.com/pull/104

--- test/utils/gatsby.test.ts
+++ test/utils/gatsby.test.ts
@@ -4,35 +4,42 @@
 import {
   CreatePageDataArgs,
   CreatePageDataType,
+  CreateBlogPageContext,
   createPageData
 } from "../../src/utils/gatsby";
 
+const baseContext = {
+  limit: 5,
+  skip: 0,
+  page: 1,
+  numPages: 1
+};
+
+const baseArgs: CreatePageDataArgs = {
+  postCount: 1,
+  type: "Language",
+  slug: "lang",
+  filterValue: "en"
+};
+
+const path = "/lang/en/";
+
+const context: CreateBlogPageContext = {
+  ...baseContext,
+  basePath: path,
+  filterValue: "en",
+  type: "Language"
+};
+
+const expectedResult: CreatePageDataType = {
+  path,
+  context
+};
+
 describe("Test createPageData function", () => {
   test("All arguments filled", () => {
-    const args: CreatePageDataArgs = {
-      postCount: 1,
-      type: "Language",
-      slug: "lang",
-      filterValue: "en"
-    };
+    const result = createPageData(baseArgs);
 
-    const expectedResult: CreatePageDataType[] = [
-      {
-        path: "/lang/en/",
-        context: {
-          limit: 5,
-          skip: 0,
-          page: 1,
-          basePath: "/lang/en/",
-          numPages: 1,
-          filterValue: "en",
-          type: "Language"
-        }
-      }
-    ];
-
-    const result = createPageData(args);
-
-    expect(result).toMatchObject(expectedResult);
+    expect(result).toMatchObject([expectedResult]);
   });
 });

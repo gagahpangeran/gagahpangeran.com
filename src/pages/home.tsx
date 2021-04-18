058f86c2747d788a2f3cc560e4abf053f2f2c880
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-04-18 20:54:08 +0700

    fix layouting home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -14,17 +14,19 @@ const HomePage = () => {
     <Layout>
       <SEO title="Home" />
       <main className="home">
-        <StaticImage
-          src="../../static/logo.png"
-          alt="GPR Logo"
-          width={240}
-          className="home__image"
-        />
-        <h1 className="home__title">Gagah Pangeran Rosfatiputra</h1>
-        <h2 className="home__desc">Low Budget Programmer</h2>
-        <h3 className="home__email">
-          <a href={`mailto:${email}`}>{email}</a>
-        </h3>
+        <div>
+          <StaticImage
+            src="../../static/logo.png"
+            alt="GPR Logo"
+            width={240}
+            className="home__image"
+          />
+          <h1 className="home__title">Gagah Pangeran Rosfatiputra</h1>
+          <h2 className="home__desc">Low Budget Programmer</h2>
+          <h3 className="home__email">
+            <a href={`mailto:${email}`}>{email}</a>
+          </h3>
+        </div>
       </main>
     </Layout>
   );

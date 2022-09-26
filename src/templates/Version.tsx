67c3de7a50d43083ab4dea2b50bc5ce4f68dc049
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2022-09-26 22:52:10 +0700

    add simple version template page

---
+++ src/templates/Version.tsx
@@ -0,0 +1,50 @@
+// Copyright (c) Gagah Pangeran Rosfatiputra (GPR) <gpr@gagahpangeran.com>.
+// Licensed under The MIT License.
+// Read the LICENSE file in the repository root for full license text.
+
+import { GetServerData, HeadProps, PageProps } from "gatsby";
+import React from "react";
+import Layout from "../components/Layout";
+import SEO from "../components/SEO";
+
+type ServerDataProps = {
+  version: string;
+};
+
+const desc = "All changes in this release";
+
+const Version: React.FC<
+  PageProps<unknown, unknown, unknown, ServerDataProps>
+> = ({ serverData }) => {
+  const { version } = serverData;
+
+  return (
+    <Layout mainTitle={version} subTitle={desc}>
+      <main className="html">{version}</main>
+    </Layout>
+  );
+};
+
+export const Head: React.FC<HeadProps> = ({ location }) => {
+  const [, , version] = location.pathname.split("/");
+  return <SEO title={version} description={desc} />;
+};
+
+export default Version;
+
+export const getServerData: GetServerData<ServerDataProps> = async ({
+  params
+}) => {
+  const version = params?.version;
+
+  if (typeof version === "string") {
+    return {
+      status: 200,
+      props: {
+        version
+      }
+    };
+  }
+
+  throw Error("Invalid version type");
+};

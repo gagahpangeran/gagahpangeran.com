d32f58dfc3982560548aae4139e7f0cccef431e0
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2024-04-12 09:59:18 +0700

    add title prompt

--- scripts/new-blog.ts
+++ scripts/new-blog.ts
@@ -1,1 +1,18 @@
-console.log("Hello");
+import prompts from "prompts";
+
+const questions = () =>
+  prompts([
+    {
+      type: "text",
+      name: "title",
+      message: "Title",
+      initial: "New Blog"
+    }
+  ]);
+
+async function main() {
+  const response = await questions();
+  console.log(response);
+}
+
+main();

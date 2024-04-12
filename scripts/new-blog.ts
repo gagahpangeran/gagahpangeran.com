1686d162889b5da1ab1bd7eeecffdcdcda32276a
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2024-04-12 10:26:46 +0700

    make questions as args object

--- scripts/new-blog.ts
+++ scripts/new-blog.ts
@@ -7,58 +7,57 @@ function slugify(title: string) {
     .replace(/ /gi, "-");
 }
 
-const questions = () =>
-  prompts([
-    {
-      type: "text",
-      name: "title",
-      message: "Title",
-      initial: "New Blog"
-    },
-    {
-      type: "text",
-      name: "slug",
-      message: "Slug",
-      initial: slugify
-    },
-    {
-      type: "date",
-      name: "date",
-      message: "Date",
-      initial: new Date()
-    },
-    {
-      type: "list",
-      name: "tags",
-      message: "Tags",
-      initial: "Story",
-      separator: ","
-    },
-    {
-      type: "select",
-      name: "lang",
-      message: "Language",
-      choices: [
-        {
-          title: "Bahasa Indonesia",
-          value: "id"
-        },
-        {
-          title: "English",
-          value: "en"
-        }
-      ]
-    },
-    {
-      type: "confirm",
-      name: "confirm",
-      message: "Create new blog?",
-      initial: true
-    }
-  ]);
+const questions: Parameters<typeof prompts>[0] = [
+  {
+    type: "text",
+    name: "title",
+    message: "Title",
+    initial: "New Blog"
+  },
+  {
+    type: "text",
+    name: "slug",
+    message: "Slug",
+    initial: slugify
+  },
+  {
+    type: "date",
+    name: "date",
+    message: "Date",
+    initial: new Date()
+  },
+  {
+    type: "list",
+    name: "tags",
+    message: "Tags",
+    initial: "Story",
+    separator: ","
+  },
+  {
+    type: "select",
+    name: "lang",
+    message: "Language",
+    choices: [
+      {
+        title: "Bahasa Indonesia",
+        value: "id"
+      },
+      {
+        title: "English",
+        value: "en"
+      }
+    ]
+  },
+  {
+    type: "confirm",
+    name: "confirm",
+    message: "Create new blog?",
+    initial: true
+  }
+];
 
 async function main() {
-  const response = await questions();
+  const response = await prompts(questions);
   console.log(response);
 }
 

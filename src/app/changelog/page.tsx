ebb12ebe78be50535e350b59221823f92e42df42
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-01-31 08:44:45 +0700

    add new changelog page
    old changelog page last commit: b58047d9fdc98f4674f421d12f056633a7f14a9f

---
+++ src/app/changelog/page.tsx
@@ -0,0 +1,33 @@
+// Copyright (c) Gagah Pangeran Rosfatiputra (GPR) <gpr@gagahpangeran.com>.
+// Licensed under The MIT License.
+// Read the LICENSE file in the repository root for full license text
+
+import Link from "next/link";
+import { getAllReleases } from "@/utils/github";
+
+const title = "Changelog";
+const desc = "All release changelog";
+
+const renderChangelogList = (data: string[]) => {
+  if (data.length === 0) {
+    return <>There is no changelog.</>;
+  }
+
+  return (
+    <ul className="changelog__list">
+      {data.map(slug => (
+        <li key={slug} className="changelog__item">
+          <Link href={`/changelog/${slug}/`} className="changelog__link">
+            <h4>{slug}</h4>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default async function ChangelogPage() {
+  const allReleases = await getAllReleases();
+
+  return <main className="changelog">{renderChangelogList(allReleases)}</main>;
+}

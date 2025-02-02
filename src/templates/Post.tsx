1d67bffa702c7366f07c2928d8b81f8d5738772a
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-02-02 18:37:45 +0700

    change post template to use new post data

--- src/templates/Post.tsx
+++ src/templates/Post.tsx
@@ -2,105 +2,50 @@
 // Licensed under The MIT License.
 // Read the LICENSE file in the repository root for full license text.
 
+import Image from "next/image";
 import { faClock } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { graphql, HeadProps, PageProps } from "gatsby";
-import { GatsbyImage } from "gatsby-plugin-image";
-import React from "react";
-import PostLabel from "../components/PostLabel";
-import Layout from "../components/Layout";
-import PageNav from "../components/PageNav";
-import SEO from "../components/SEO";
-import ShareButton from "../components/ShareButton";
-import { getPostData } from "../utils/data";
+import ReactMarkdown from "react-markdown";
+import PostLabel from "@/components/PostLabel";
+import ShareButton from "@/components/ShareButton";
+import PageNav from "@/components/PageNav";
+import { getOlderNewerPost, type PostData } from "@/utils/post";
+import Page from "./Page";
 
-const Post: React.FC<PageProps<Queries.PostTemplateQuery>> = ({ data }) => {
-  const siteUrl = data.site?.siteMetadata.siteUrl;
-  const { title, date, datetime, tags, lang, slug, imageUrl, image, html } =
-    getPostData(data.post);
+interface Props {
+  post: PostData;
+}
 
-  if (siteUrl == null) {
-    throw Error("siteUrl not found");
-  }
-
-  if (image == null) {
-    throw Error(`Image of post '${title}' not found`);
-  }
-
-  const newerPost = data.newerPost ? getPostData(data.newerPost) : null;
-  const olderPost = data.olderPost ? getPostData(data.olderPost) : null;
+export default function Post({ post }: Props) {
+  const { title, date, datetime, tags, lang, image, content, slug } = post;
+  const otherPost = getOlderNewerPost(slug);
 
   return (
-    <Layout mainTitle={title}>
+    <Page mainTitle={title}>
       <article className="post">
         <time className="post__date" dateTime={datetime}>
           <FontAwesomeIcon icon={faClock} />
           {date}
         </time>
         <PostLabel tags={tags} lang={lang} />
-        <ShareButton link={`${siteUrl}${slug}`} title={title} />
-        <a href={imageUrl} target="_blank" rel="noopener noreferrer">
-          <GatsbyImage
-            class="post__featured-image"
-            image={image}
+        <ShareButton link={slug} title={title} />
+        <a href={image.src} target="_blank" rel="noopener noreferrer">
+          <Image
+            {...image}
+            className="post__featured-image"
             alt={title}
             title={title}
           />
         </a>
-        <main className="markdown" dangerouslySetInnerHTML={{ __html: html }} />
+        <ReactMarkdown className="markdown">{content}</ReactMarkdown>
       </article>
 
-      <PageNav newerData={newerPost} olderData={olderPost} suffix="Post" />
-    </Layout>
+      {otherPost && (
+        <PageNav
+          newerData={otherPost.newerPost}
+          olderData={otherPost.olderPost}
+        />
+      )}
+    </Page>
   );
-};
-
-export const Head: React.FC<HeadProps<Queries.PostTemplateQuery>> = ({
-  data
-}) => {
-  const { title, description, imageUrl } = getPostData(data.post);
-  return <SEO title={title} description={description} thumbnail={imageUrl} />;
-};
-
-export default Post;
-
-export const pageQuery = graphql`
-  query PostTemplate($id: String!, $newerId: String, $olderId: String) {
-    post: markdownRemark(id: { eq: $id }) {
-      ...PostDetail
-    }
-    newerPost: markdownRemark(id: { eq: $newerId }) {
-      ...PostDetail
-    }
-    olderPost: markdownRemark(id: { eq: $olderId }) {
-      ...PostDetail
-    }
-    site {
-      siteMetadata {
-        siteUrl
-      }
-    }
-  }
-
-  fragment PostDetail on MarkdownRemark {
-    id
-    html
-    fields {
-      slug
-    }
-    excerpt
-    frontmatter {
-      title
-      date(formatString: "MMMM DD, YYYY")
-      datetime: date
-      tags
-      lang
-      featuredImage {
-        publicURL
-        childImageSharp {
-          gatsbyImageData(width: 800, layout: CONSTRAINED, placeholder: BLURRED)
-        }
-      }
-    }
-  }
-`;
+}

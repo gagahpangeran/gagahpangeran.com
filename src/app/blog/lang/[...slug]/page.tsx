8aac18f3ed73fe0d638991928a77eeb2659bcd59
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-02-02 22:04:16 +0700

    add all post lang page

---
+++ src/app/blog/lang/[...slug]/page.tsx
@@ -0,0 +1,65 @@
+// Copyright (c) Gagah Pangeran Rosfatiputra (GPR) <gpr@gagahpangeran.com>.
+// Licensed under The MIT License.
+// Read the LICENSE file in the repository root for full license text.
+
+import { type Metadata } from "next";
+import Blog from "@/templates/Blog";
+import { getBlogMetaData } from "@/utils/data";
+import { getAllPosts } from "@/utils/post";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: Promise<{ slug: string[] }>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params;
+  const value = slug[0];
+
+  const { title, desc } = getBlogMetaData({
+    type: "Language",
+    filterValue: value
+  });
+
+  return {
+    title,
+    description: desc
+  };
+}
+
+export default async function BlogTag({ params }: Props) {
+  const { slug } = await params;
+
+  if (slug.length > 2) {
+    notFound();
+  }
+
+  const value = slug[0];
+  const page = Number(slug[1] ?? 1);
+
+  if (isNaN(page) || page < 0) {
+    notFound();
+  }
+
+  const { posts, totalPage } = getAllPosts({
+    page,
+    type: "lang",
+    value
+  });
+
+  const { pageTitle, pageDesc } = getBlogMetaData({
+    type: "Language",
+    filterValue: value
+  });
+
+  return (
+    <Blog
+      title={pageTitle}
+      description={pageDesc}
+      posts={posts}
+      pageNumber={page}
+      totalPage={totalPage}
+      paginationPath={`/blog/lang/${slug[0]}/`}
+    />
+  );
+}

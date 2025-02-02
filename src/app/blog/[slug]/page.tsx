1d2e7d579a6923469eb18c58dd620453cd93a608
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-02-02 18:37:56 +0700

    handle blog post routing

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,9 +3,11 @@
 // Read the LICENSE file in the repository root for full license text.
 
 import { type Metadata } from "next";
-import { notFound } from "next/navigation";
 import { getBlogMetaData } from "@/utils/data";
+import BlogPost from "@/templates/Post";
 import Blog from "../page";
+import { getPostBySlug } from "@/utils/post";
+import { notFound } from "next/navigation";
 
 export async function generateMetadata(): Promise<Metadata> {
   const { title, desc } = getBlogMetaData({ type: "Blog", filterValue: "" });
@@ -24,7 +26,13 @@ export default async function BlogPagination({
   const page = Number(slug);
 
   if (isNaN(page) || page < 0) {
-    notFound();
+    const postData = getPostBySlug(slug);
+
+    if (postData == null) {
+      notFound();
+    }
+
+    return <BlogPost post={postData} />;
   }
 
   return <Blog page={page} />;

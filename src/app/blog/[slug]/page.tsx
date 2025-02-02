8b918e274a951294d98910248660ab974e3993dc
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-02-02 18:54:30 +0700

    add metadata for blog post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,13 +3,32 @@
 // Read the LICENSE file in the repository root for full license text.
 
 import { type Metadata } from "next";
-import { getBlogMetaData } from "@/utils/data";
-import BlogPost from "@/templates/Post";
-import Blog from "../page";
-import { getPostBySlug } from "@/utils/post";
 import { notFound } from "next/navigation";
+import BlogPost from "@/templates/Post";
+import { getBlogMetaData } from "@/utils/data";
+import { getPostBySlug } from "@/utils/post";
+import Blog from "../page";
 
-export async function generateMetadata(): Promise<Metadata> {
+interface Props {
+  params: Promise<{ slug: string }>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params;
+  const page = Number(slug);
+
+  if (isNaN(page) || page < 0) {
+    const postData = getPostBySlug(slug);
+
+    if (postData != null) {
+      const { title, description } = postData;
+      return {
+        title,
+        description
+      };
+    }
+  }
+
   const { title, desc } = getBlogMetaData({ type: "Blog", filterValue: "" });
   return {
     title,
@@ -17,11 +36,7 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-export default async function BlogPagination({
-  params
-}: {
-  params: Promise<{ slug: string }>;
-}) {
+export default async function BlogPagination({ params }: Props) {
   const { slug } = await params;
   const page = Number(slug);
 

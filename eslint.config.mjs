dbefe345c75bbab0c1355b290a4ff4754c1365bc
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-02-06 12:56:55 +0700

    add new eslint flat config for nextjs

---
+++ eslint.config.mjs
@@ -0,0 +1,48 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslintPluginPrettierRecommended,
+  {
+    ignores: [
+      "node_modules/*",
+      ".next/*",
+      ".cache/*",
+      "public/*",
+      ".github/*",
+      "types/",
+      "coverage/",
+      ".netlify/*"
+    ]
+  },
+  {
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true
+        }
+      ]
+    }
+  }
+];
+
+export default eslintConfig;

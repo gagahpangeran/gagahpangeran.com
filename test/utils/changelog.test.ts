b61b8797b09dbf7930231679c921bf829d625b15
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-02-06 22:00:35 +0700

    update jest toThrow method

--- test/utils/changelog.test.ts
+++ test/utils/changelog.test.ts
@@ -35,23 +35,19 @@ describe("Test getGithubConstants function", () => {
   test("USER env variable is not set", () => {
     delete process.env.GITHUB_USERNAME;
 
-    expect(() => getGithubConstants()).toThrowError(
-      "Github username not found"
-    );
+    expect(() => getGithubConstants()).toThrow("Github username not found");
   });
 
   test("REPO env variable is not set", () => {
     delete process.env.GITHUB_REPO;
 
-    expect(() => getGithubConstants()).toThrowError(
-      "Github repository not found"
-    );
+    expect(() => getGithubConstants()).toThrow("Github repository not found");
   });
 
   test("TOKEN env variable is not set", () => {
     delete process.env.GITHUB_TOKEN;
 
-    expect(() => getGithubConstants()).toThrowError("Github token not found");
+    expect(() => getGithubConstants()).toThrow("Github token not found");
   });
 });
 

66e34443c131d297ccbace05efc0d541fc735a03
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2025-02-09 18:02:20 +0700

    move render function in changelog page

--- src/app/changelog/page.tsx
+++ src/app/changelog/page.tsx
@@ -9,30 +9,26 @@ import { getAllReleases } from "@/utils/changelog";
 const title = "Changelog";
 const desc = "All release changelog";
 
-const renderChangelogList = (data: string[]) => {
-  if (data.length === 0) {
-    return <>There is no changelog.</>;
-  }
-
-  return (
-    <ul className="changelog__list">
-      {data.map(slug => (
-        <li key={slug} className="changelog__item">
-          <Link href={`/changelog/${slug}/`} className="changelog__link">
-            <h4>{slug}</h4>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
-};
-
 export default async function ChangelogPage() {
   const allReleases = await getAllReleases();
 
   return (
     <Page mainTitle={title} subTitle={desc}>
-      <main className="changelog">{renderChangelogList(allReleases)}</main>;
+      <main className="changelog">
+        {allReleases.length === 0 ? (
+          <h3>There is no changelog.</h3>
+        ) : (
+          <ul className="changelog__list">
+            {allReleases.map(slug => (
+              <li key={slug} className="changelog__item">
+                <Link href={`/changelog/${slug}/`} className="changelog__link">
+                  <h4>{slug}</h4>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </main>
     </Page>
   );
 }
